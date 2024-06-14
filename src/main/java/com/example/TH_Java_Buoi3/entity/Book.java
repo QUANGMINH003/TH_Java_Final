0d15febd8d74a8a886d8247449d86647a83fa1ef
QUANGMINH003 <minhkhietnguyen1910@gmail.com>
2024-06-14 15:37:42 +0700

    Hoan thanh Login va Register

--- src/main/java/com/example/TH_Java_Buoi3/entity/Book.java
+++ src/main/java/com/example/TH_Java_Buoi3/entity/Book.java
@@ -1,9 +1,11 @@
 package com.example.TH_Java_Buoi3.entity;
 
 import com.example.TH_Java_Buoi3.validator.annotation.ValidCategoryId;
+import com.example.TH_Java_Buoi3.validator.annotation.ValidUserId;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -21,22 +23,29 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "title")
-    @NotEmpty(message = "Title must not be emty")
+    @Column(name = "title", length = 50)
+    @NotEmpty(message = "Title must not be null")
     @Size(max = 50, min = 1, message = "Title must be less than 50 characters")
     private String title;
 
-    @Column(name = "author")
+    @Column(name = "author", length = 50)
+    @Size(max = 50, min = 1, message = "Author must be less than 50 characters")
     private String author;
 
     @Column(name = "price")
     @NotNull(message = "Price is required")
+    @Positive(message = "Price must be greater than 0")
     private Double price;
 
     @ManyToOne
     @JoinColumn(name ="category_id")
     @ValidCategoryId
     private Category category;
+
+    @ManyToOne
+    @JoinColumn(name ="user_id", referencedColumnName = "id")
+    @ValidUserId
+    private User user;
 
 
 }
